30e2d42b9dbecea05ce9ae966e3643e7bc86e08e
Aka Mitch <newmitch@gmail.com>
2024-03-30 16:14:11 -0300

    2

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Test GeoIP</h1>
 <?php
 require_once 'vendor/autoload.php';
 use GeoIp2\Database\Reader;
@@ -10,4 +19,6 @@ $record = $reader->city('128.101.101.101');
 
 print($record->country->isoCode);
 print($record);
-?>
+?>    
+</body>
+</html>
