c8ed841e3fe62a3061e97ac4498405babf0bd9e3
Aka Mitch <newmitch@gmail.com>
2024-03-30 16:32:52 -0300

    2

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
     <title>GeoIP Mitch test</title>
 </head>
 <body>
-    <h1>Test GeoIP 2</h1>
+    <h1>Test GeoIP</h1>
+    <p>
+        If country = US and Subdivision isoCode = DE, will be redirected to http://delaver.yahoo.com
+    </p>
 <?php
 require_once 'vendor/autoload.php';
 use GeoIp2\Database\Reader;
@@ -18,10 +21,19 @@ $reader = new Reader('GeoLite2-City.mmdb');
 $clientIP = $_SERVER['HTTP_CF_CONNECTING_IP'];
 
 $record = $reader->city($clientIP);
-
-print("Country: " . $record->country->isoCode . "<br>\n"); // 'US'
-print("Subdivision isoCode: " . $record->mostSpecificSubdivision->isoCode . "<br>\n"); // 'MN'
+$country = $record->country->isoCode;
+$subdivision = $record->mostSpecificSubdivision->isoCode;
+print("Country: " . $country . "<br>\n"); // 'US'
+print("Subdivision isoCode: " . $subdivision . "<br>\n"); // 'MN'
 print("Subdivision name: " . $record->mostSpecificSubdivision->name . "<br>\n"); // 'Minnesota'
+
+if ($country == "US" && $subdivision == "DE") {
+    $newURL = "http://delaver.yahoo.com";
+    header("Location: $newURL");
+}
+
+
+
 ?>    
 </body>
 </html>
