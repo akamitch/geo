10cd226ef1fc73c19c18bc05f0af57b9ddcd868f
Aka Mitch <newmitch@gmail.com>
2024-03-30 15:52:51 -0300

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'vendor/autoload.php';
+use GeoIp2\Database\Reader;
+
+// This reader object should be reused across lookups as creation of it is
+// expensive.
+$reader = new Reader('GeoLite2-City.mmdb');
+
+$record = $reader->city('128.101.101.101');
+
+print($record->country->isoCode);
+?>
